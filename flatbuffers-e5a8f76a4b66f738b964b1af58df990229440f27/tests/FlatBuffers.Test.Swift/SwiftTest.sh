f799361192c60267061f5ff36cfe7e338b7d29bc
zzhou292@wisc.edu <zzhou292@wisc.edu>
2020-07-07 23:33:02 -0500

    isdksjdsjd

---
+++ flatbuffers-e5a8f76a4b66f738b964b1af58df990229440f27/tests/FlatBuffers.Test.Swift/SwiftTest.sh
@@ -0,0 +1,9 @@
+swift_dir=`pwd`
+cd ..
+test_dir=`pwd`
+
+cd ${swift_dir}/Tests/FlatBuffers.Test.SwiftTests
+${test_dir}/../flatc --swift --gen-mutable --grpc --gen-object-api -I ${test_dir}/include_test ${test_dir}/monster_test.fbs ${test_dir}/union_vector/union_vector.fbs
+cd ${swift_dir}
+swift build --build-tests
+swift test
